1805bd51707a6581e772dcaacc7d5160782c0393
EliasKristoffer <eliasks@stud.ntnu.no>
2020-09-21 14:40:31 +0200

    Add boto3 requirement, simple scan dynamodb view

--- backend/trckpck/app/views.py
+++ backend/trckpck/app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+import boto3
 
 
 
 def data_dump(request):
-    data = {
-        'test':123
-    }
-    return JsonResponse(data)
+    dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
+    table = dynamodb.Table('test_table')
+    data = table.scan()
+    items = data.get('Items', [])
+
+    return JsonResponse(items, safe=False)
