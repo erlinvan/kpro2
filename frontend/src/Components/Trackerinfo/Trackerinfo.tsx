cad9faae7126374ecdb7ac8d49abf33b61faf6a6
Erling Van De Weijer <erling.weijer@gmail.com>
2020-09-23 18:27:51 +0200

    add mockdata for trackerinfo and display the data

--- frontend/src/Components/Trackerinfo/Trackerinfo.tsx
+++ frontend/src/Components/Trackerinfo/Trackerinfo.tsx
@@ -1,7 +1,91 @@
+import {
+    Box,
+    Card,
+    CardContent,
+    Container,
+    Grid,
+    Typography,
+} from '@material-ui/core'
 import React from 'react'
 
 const Trackerinfo = () => {
-    return <h1>Trackerinfo</h1>
+    const dummy1: trackerifno = {
+        name: 'Dummy1',
+        maxHumidity: '2',
+        minHumidity: '1',
+        maxTemperature: '42C',
+        minTemperature: '23C',
+        maxImpact: '25 m/s^2',
+    }
+    return (
+        <>
+            <Container>
+                <Box mt={5}>
+                    <Card>
+                        <Grid container xs={12}>
+                            <Grid item xs={4}>
+                                <CardContent>
+                                    <Typography variant="body2" component="h2">
+                                        <b>Name:</b> {dummy1.name}
+                                    </Typography>
+                                </CardContent>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <CardContent>
+                                    <Typography variant="body2" component="h2">
+                                        <b>Max temperature:</b>{' '}
+                                        {dummy1.maxTemperature}
+                                    </Typography>
+                                </CardContent>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <CardContent>
+                                    <Typography variant="body2" component="h2">
+                                        <b>Max humidity:</b>{' '}
+                                        {dummy1.maxHumidity}
+                                    </Typography>
+                                </CardContent>
+                            </Grid>
+                        </Grid>
+                        <Grid container xs={12}>
+                            <Grid item xs={4}>
+                                <CardContent>
+                                    <Typography variant="body2" component="h2">
+                                        <b>Max impact:</b> {dummy1.maxImpact}
+                                    </Typography>
+                                </CardContent>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <CardContent>
+                                    <Typography variant="body2" component="h2">
+                                        <b>Min temperature:</b>{' '}
+                                        {dummy1.minTemperature}
+                                    </Typography>
+                                </CardContent>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <CardContent>
+                                    <Typography variant="body2" component="h2">
+                                        <b>Min humidity:</b>{' '}
+                                        {dummy1.minHumidity}
+                                    </Typography>
+                                </CardContent>
+                            </Grid>
+                        </Grid>
+                    </Card>
+                </Box>
+            </Container>
+        </>
+    )
+}
+
+interface trackerifno {
+    name: string
+    maxImpact: string
+    maxTemperature: string
+    minTemperature: string
+    maxHumidity: string
+    minHumidity: string
 }
 
 export default Trackerinfo
