62b7ca9a2d661e1d56bd05f9548ccca913fa3977
EliasKristoffer <eliasks@stud.ntnu.no>
2020-09-23 11:46:16 +0200

    Create tests for the views

--- backend/trckpck/app/tests.py
+++ backend/trckpck/app/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from unittest.mock import patch
 
-# Create your tests here.
+from trckpck.app.models import Package
+import json
+
+
+class PackageTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        for i in range(6):
+            Package.objects.create(company_owner='apple', tracker_id='123')
+        for i in range(4):
+            Package.objects.create(company_owner='komplett', tracker_id='111')
+
+    def dummy_data(self):
+        return 'test'
+
+    @patch.object(Package, 'get_package_data', dummy_data)
+    def test_package_dump(self):
+        res = self.client.get(reverse('package_dump'), format='JSON')
+        self.assertEqual(len(json.loads(res.content)), 10)
+
+    @patch.object(Package, 'get_package_data', dummy_data)
+    def test_get_packages_data_by_company_id(self):
+        res = self.client.get(reverse('get_packages_data_by_company_id', args=['apple']), format='JSON')
+        self.assertEqual(len(json.loads(res.content)), 6)
+
+    @patch.object(Package, 'get_package_data', dummy_data)
+    def test_get_package_data_by_package_id(self):
+        id = Package.objects.all()[0].pk
+        res = self.client.get(reverse('get_package_data_by_package_id', args=[id]), format='JSON')
+        res2 = self.client.get(reverse('get_package_data_by_package_id', args=[9000]), format='JSON')
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res2.status_code, 404)
