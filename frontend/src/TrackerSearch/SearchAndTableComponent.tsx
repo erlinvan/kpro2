234cbde1f18a3eebac6c6346bacb40db99a98da3
Ask Sommervoll <asksom@gmail.com>
2020-09-23 11:18:06 +0200

    Add searchfield and table for trackers. Only visuals

---
+++ frontend/src/TrackerSearch/SearchAndTableComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SearchBarAndTitle from './SearchBarAndTitle'
+import TrackerTable from './TrackerTable'
+
+
+const SearchAndTableComponent = ()=>
+{
+    return (
+        <div>
+            <SearchBarAndTitle/>
+            <TrackerTable/>
+        </div>
+    )
+}
+
+export default SearchAndTableComponent;
