a923c96235c7c4b92308f8c521eb42d720b0f075
Erling Van De Weijer <erling.weijer@gmail.com>
2020-09-23 11:10:26 +0200

    add routing for pages: login, trackerinfo and trackers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import './App.css'
+import {
+    BrowserRouter,
+    Route,
+    Router,
+    Switch,
+    withRouter,
+} from 'react-router-dom'
 import AppBar from './AppBar/AppBar'
+import Trackerinfo from './Components/Trackerinfo/Trackerinfo'
+import Trackers from './Components/Trackerinfo/Trackers/Trackers'
+import Login from './Components/Trackerinfo/Login/Login'
 
 function App() {
     return (
-        <div className="App">
-            <AppBar></AppBar>
-        </div>
+        <>
+            <div className="App">
+                <AppBar></AppBar>
+            </div>
+            <div>
+                <BrowserRouter>
+                    <Switch>
+                        <Route
+                            exact
+                            path="/trackerinfo"
+                            component={Trackerinfo}
+                        />
+                        <Route exact path="/trackers" component={Trackers} />
+                        <Route exact path="/login" component={Login} />
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        </>
     )
 }
 
---
+++ frontend/src/Components/Trackerinfo/Trackers/Trackers.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Trackers = () => {
+    return <h1>Trackers</h1>
+}
+
+export default Trackers
