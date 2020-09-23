4cfb2ab7ef54c5f6016f209dfda7abcaa1aec67f
Ask Sommervoll <asksom@gmail.com>
2020-09-23 09:57:08 +0200

    Add AppBar w/ text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import AppBar from './AppBar/AppBar'
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppBar></AppBar>
+        </div>
+    )
 }
 
-export default App;
+export default App
