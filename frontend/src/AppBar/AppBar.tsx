426a8f8018623585ed4c4378db8fa522883a712c
Ask Sommervoll <asksom@gmail.com>
2020-09-23 09:57:08 +0200

    Add AppBar w/ text

---
+++ frontend/src/AppBar/AppBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AppBarMUI from '@material-ui/core/AppBar'
+import Typography from '@material-ui/core/Typography'
+
+
+const AppBar = () => {
+
+    return (
+        <AppBarMUI position="static">
+            <Typography variant="h6">
+                trckpck
+            </Typography>
+        </AppBarMUI>
+    )
+}
+
+export default AppBar
