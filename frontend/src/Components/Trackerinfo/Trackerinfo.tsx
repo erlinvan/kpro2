25f32b71586fc284df7f51e548ae39ca95e61f2f
Erling Van De Weijer <erling.weijer@gmail.com>
2020-09-25 12:20:06 +0200

    Add mockdata and create charts for temperature and humidity

--- frontend/src/Components/Trackerinfo/Trackerinfo.tsx
+++ frontend/src/Components/Trackerinfo/Trackerinfo.tsx
@@ -7,15 +7,27 @@ import {
     Typography,
 } from '@material-ui/core'
 import React from 'react'
+import Charts from '../Charts/Charts'
 
 const Trackerinfo = () => {
-    const dummy1: trackerifno = {
+    //Place holders until we can fetch data from API.
+    const dummy1: trackerinfo = {
         name: 'Dummy1',
         maxHumidity: '2',
         minHumidity: '1',
         maxTemperature: '42C',
         minTemperature: '23C',
         maxImpact: '25 m/s^2',
+    }
+    const temperatureChart = {
+        yAxis: [1, 2, 1, 4, 3, 6],
+        xAxis: ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00'],
+        type: 'Temperature',
+    }
+    const humidityChart = {
+        yAxis: [15, 16, 27, 20, 18, 19],
+        xAxis: ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00'],
+        type: 'Humidity',
     }
     return (
         <>
@@ -75,11 +87,13 @@ const Trackerinfo = () => {
                     </Card>
                 </Box>
             </Container>
+            <Charts data={temperatureChart} />
+            <Charts data={humidityChart} />
         </>
     )
 }
 
-interface trackerifno {
+interface trackerinfo {
     name: string
     maxImpact: string
     maxTemperature: string
