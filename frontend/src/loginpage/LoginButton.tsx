59c2dda347f52d773892e2badbd05c6882d3843a
anjarand <anjarand92@gmail.com>
2020-09-28 12:16:56 +0200

    Removed spaces

--- frontend/src/loginpage/LoginButton.tsx
+++ frontend/src/loginpage/LoginButton.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 
 const LoginButton= () => {
-
     return(
-
         <Button variant="contained">LogIn</Button>
-
     )
-
 }
 
 export default LoginButton; 
--- frontend/src/loginpage/PasswordComp.tsx
+++ frontend/src/loginpage/PasswordComp.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField'
 
-
-
-
 const PasswordComp= () => {
     return(
         <TextField id="standard-basic" label="Password" />
-
     );
-
-
-
-
-
 }
 
 export default PasswordComp; 
