d7a09b413608418c9ba1dd4031b958f78b614718
Erling Van De Weijer <erling.weijer@gmail.com>
2020-09-28 11:18:19 +0200

    Create new component for information in cards, to simplify code

---
+++ frontend/src/Components/Trackerinfo/CardInfo/CardInfo.tsx
@@ -0,0 +1,22 @@
+import { CardContent, Grid, Typography } from '@material-ui/core'
+import React from 'react'
+
+interface IProps {
+    value: string
+    parameter: string
+}
+
+const CardInfo = ({ value, parameter }: IProps) => {
+    console.log(value)
+    return (
+        <Grid item xs={4}>
+            <CardContent>
+                <Typography variant="body2" component="h2">
+                    <b>{parameter}:</b> {value}
+                </Typography>
+            </CardContent>
+        </Grid>
+    )
+}
+
+export default CardInfo
