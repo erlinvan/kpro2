355eaa306eb5c4c0a963823b619d988beb2773ee
Ask Sommervoll <asksom@gmail.com>
2020-09-28 18:35:44 +0200

    Add markers to map, also clickable

--- frontend/src/Components/TrackerSearch/TrackerMap.tsx
+++ frontend/src/Components/TrackerSearch/TrackerMap.tsx
@@ -1,23 +1,46 @@
-import React from 'react'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
-import { Icon } from 'leaflet'
+import React, { useEffect, useState } from 'react'
+import { Map, Marker, TileLayer } from 'react-leaflet'
+
 import './SearchAndTableComponent.css'
 
+type props = {
+    data: any,
+}
 
-const TrackerMap = () => {
+const TrackerMap = ({ data }: props) => {
+
+
+    const [markers, setMarkers] = useState([])
+
+    useEffect(() => {
+        setMarkers(data.map((trackerLocation: { id: any; coordinates: any[] }) =>
+            <Marker
+                key={trackerLocation.id}
+                position={[trackerLocation.coordinates[0], trackerLocation.coordinates[1]]}
+                onClick={() => {
+                    console.log(trackerLocation.id)
+                }}
+            />,
+        ))
+    }, [data])
+
 
     return (
         <div>
-            <Map  center={[59.9139, 10.7522]} zoom={6}>
+            <Map center={[64.9139, 18.7522]} zoom={4.8}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 
                     attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 />
+                {
+                    markers
+                }
+
             </Map>
         </div>
     )
 }
 
 
-export default TrackerMap;
+export default TrackerMap
