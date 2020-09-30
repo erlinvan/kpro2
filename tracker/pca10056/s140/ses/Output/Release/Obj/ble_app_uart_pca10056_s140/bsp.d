6010c5ed1f8e20ff280a443ae0ded3013aefd784
Mathias Chunnoo <mathiaschunnoo@Mathiass-MacBook-Pro.local>
2020-09-30 12:52:15 +0200

    Changes base to ble_app_uart example

---
+++ tracker/pca10056/s140/ses/Output/Release/Obj/ble_app_uart_pca10056_s140/bsp.d
@@ -0,0 +1,66 @@
+Output/Release/Obj/ble_app_uart_pca10056_s140/bsp.o: \
+ /Users/mathiaschunnoo/Desktop/Skole/ntnu/kpro/kpro2/tracker/nRF5_SDK_17.0.0_9d13099/components/libraries/bsp/bsp.c \
+ /Users/mathiaschunnoo/Desktop/Skole/ntnu/kpro/kpro2/tracker/nRF5_SDK_17.0.0_9d13099/components/libraries/bsp/bsp.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10b/include/stdint.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10b/include/stdbool.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/boards/boards.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/hal/nrf_gpio.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/nrfx.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/integration/nrfx/nrfx_config.h \
+ ../config/sdk_config.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/drivers/nrfx_common.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10b/include/stddef.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10b/include/__crossworks.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf52840.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/core_cm4.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/system_nrf52840.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf_peripherals.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf52840_peripherals.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/integration/nrfx/nrfx_glue.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/integration/nrfx/legacy/apply_old_config.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/soc/nrfx_irqs.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/soc/nrfx_irqs_nrf52840.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/util/nrf_assert.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/util/app_util.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/util/nordic_common.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/softdevice/s140/headers/nrf52/nrf_mbr.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/softdevice/s140/headers/nrf_svc.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/util/nordic_common.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/util/app_util_platform.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/softdevice/s140/headers/nrf_soc.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/softdevice/s140/headers/nrf_svc.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/softdevice/s140/headers/nrf_error.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/softdevice/s140/headers/nrf_error_soc.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/softdevice/s140/headers/nrf_nvic.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/util/nrf_assert.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/util/app_error.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10b/include/stdio.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/util/sdk_errors.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/softdevice/s140/headers/nrf_error.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/util/app_error_weak.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/soc/nrfx_coredep.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/soc/nrfx_atomic.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/util/sdk_errors.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/util/sdk_resources.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/softdevice/s140/headers/nrf_sd_def.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/softdevice/s140/headers/nrf_soc.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/drivers/nrfx_errors.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/boards/pca10056.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/button/app_button.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/util/app_error.h \
+ /Users/mathiaschunnoo/Desktop/Skole/ntnu/kpro/kpro2/tracker/nRF5_SDK_17.0.0_9d13099/components/libraries/bsp/bsp_config.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/timer/app_timer.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/log/nrf_log_instance.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/experimental_section_vars/nrf_section.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/log/nrf_log_types.h \
+ ../../../nRF5_SDK_17.0.0_9d13099/components/libraries/sortlist/nrf_sortlist.h
---
+++ tracker/pca10056/s140/ses/Output/Release/Obj/ble_app_uart_pca10056_s140/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/Release/Obj/ble_app_uart_pca10056_s140/ses_startup_nrf_common.o: \
+ /Users/mathiaschunnoo/Desktop/Skole/ntnu/kpro/kpro2/tracker/nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/ses_startup_nrf_common.s
