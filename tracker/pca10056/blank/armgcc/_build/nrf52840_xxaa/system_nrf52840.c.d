26880a886c7641d098b86ab3a64fde3ab4e95488
Mathias Chunnoo <mathiaschunnoo@Mathiass-MacBook-Pro.local>
2020-09-30 09:46:04 +0200

    Changed project structure

---
+++ tracker/pca10056/blank/armgcc/_build/nrf52840_xxaa/system_nrf52840.c.d
@@ -0,0 +1,77 @@
+_build/nrf52840_xxaa/system_nrf52840.c.o: \
+ ../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/system_nrf52840.c \
+ /usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stdint.h \
+ /usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/arm-none-eabi/include/stdint.h \
+ /usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/arm-none-eabi/include/sys/features.h \
+ /usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/arm-none-eabi/include/sys/_stdint.h \
+ /usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf52840.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/system_nrf52840.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf_erratas.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf51_erratas.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf52_erratas.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf53_erratas.h \
+ ../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf91_erratas.h
+
+/usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stdint.h:
+
+/usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/arm-none-eabi/include/stdint.h:
+
+/usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/arm-none-eabi/include/sys/features.h:
+
+/usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/arm-none-eabi/include/_newlib_version.h:
+
+/usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/arm-none-eabi/include/sys/_stdint.h:
+
+/usr/local/Cellar/arm-gcc-bin/7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf52840.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/cmsis_version.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/cmsis_compiler.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include/mpu_armv7.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/system_nrf52840.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf52840_bitfields.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf51_to_nrf52840.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf52_to_nrf52840.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf_erratas.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf51_erratas.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf52_erratas.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf53_erratas.h:
+
+../../../../nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk/nrf91_erratas.h:
