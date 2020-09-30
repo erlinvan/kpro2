840cb16284d4dcb0a2e545185a56d8e293fb1345
Erling Van De Weijer <erling.weijer@gmail.com>
2020-09-30 15:05:05 +0200

    Create custom fetch method to abstract fetching logic from other parts of the code

---
+++ frontend/src/utils/useFetch.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+
+const useFetch = <T extends any>(endpoint: string) => {
+    const [response, setResponse] = useState<T>()
+    const [error, setError] = useState('')
+
+    useEffect(() => {
+        let isSubscribed = true
+        const fetchData = async () => {
+            try {
+                const res = await fetch(`${apiUrl}/${endpoint}`)
+                if (!res.ok) {
+                    throw new Error(`${res.status} ${res.statusText}`)
+                }
+                const json = await res.json()
+                if (!isSubscribed) {
+                    return
+                }
+                setResponse(json)
+            } catch (error) {
+                setError(error.message)
+            }
+        }
+
+        fetchData()
+        return () => {
+            isSubscribed = false
+        }
+    }, [endpoint])
+
+    return { response, error }
+}
+
+export const apiUrl = 'https://api.trckpck.theodorc.no'
+
+export default useFetch
