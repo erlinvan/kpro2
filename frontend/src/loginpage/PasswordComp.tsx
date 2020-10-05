21f2886be4d0aff32814f383c2d02e6829fefb52
Ask Sommervoll <asksom@gmail.com>
2020-10-05 10:05:23 +0200

    Make password illegible

--- frontend/src/loginpage/PasswordComp.tsx
+++ frontend/src/loginpage/PasswordComp.tsx
@@ -3,7 +3,7 @@ import TextField from '@material-ui/core/TextField'
 
 const PasswordComp= () => {
     return(
-        <TextField id="standard-basic" label="Password" />
+        <TextField type="password" id="standard-basic" label="Password" />
     );
 }
 
