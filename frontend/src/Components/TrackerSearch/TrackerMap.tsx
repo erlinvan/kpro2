427173cb4370b176b208c5efb39b9ff83a0f598e
Erling Van De Weijer <erling.weijer@gmail.com>
2020-10-05 15:06:34 +0200

    Fetch data from API for Trackers page, and remove mockdata

--- frontend/src/Components/TrackerSearch/TrackerMap.tsx
+++ frontend/src/Components/TrackerSearch/TrackerMap.tsx
@@ -1,46 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import { Map, Marker, TileLayer } from 'react-leaflet'
-
+import { IGPSData, ITrackers } from '../../Interfaces/ITrackers'
 import './SearchAndTableComponent.css'
 
 type props = {
-    data: any,
+    data: ITrackers[]
 }
 
 const TrackerMap = ({ data }: props) => {
-
-
-    const [markers, setMarkers] = useState([])
+    const [markers, setMarkers] = useState<any>([])
 
     useEffect(() => {
-        setMarkers(data.map((trackerLocation: { id: any; coordinates: any[] }) =>
-            <Marker
-                key={trackerLocation.id}
-                position={[trackerLocation.coordinates[0], trackerLocation.coordinates[1]]}
-                onClick={() => {
-                    console.log(trackerLocation.id)
-                }}
-            />,
-        ))
+        setMarkers(
+            data.map((trackerLocation: { id: string; gps: IGPSData }) => (
+                <Marker
+                    key={trackerLocation.id}
+                    position={[
+                        trackerLocation.gps.lat,
+                        trackerLocation.gps.lon,
+                    ]}
+                    onClick={() => {
+                        console.log(trackerLocation.id)
+                    }}
+                />
+            ))
+        )
     }, [data])
-
 
     return (
         <div>
             <Map center={[64.9139, 18.7522]} zoom={4.8}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-
-                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
-                {
-                    markers
-                }
-
+                {markers}
             </Map>
         </div>
     )
 }
-
 
 export default TrackerMap
