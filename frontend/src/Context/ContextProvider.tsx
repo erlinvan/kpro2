c337c470e28ce1e0aab4ffa8f9d2ff2e5ae765bd
Ask Sommervoll <asksom@gmail.com>
2020-10-19 09:34:56 +0200

    Add user to context and change login logic (#18)
    * Add user to context and change login logic
    
    * Fix code-standard _errors_

--- frontend/src/Context/ContextProvider.tsx
+++ frontend/src/Context/ContextProvider.tsx
@@ -3,13 +3,18 @@ import React, { Dispatch, SetStateAction, useState } from 'react'
 export const Context = React.createContext<IGlobalContext>({
     isLoggedIn: false,
     setIsLoggedIn: () => {},
+    userName: '',
+    setUserName:()=>{},
     trackerID: 0,
     setTrackerID: () => {},
+
 })
 
 interface IGlobalContext {
     isLoggedIn: boolean,
-    setIsLoggedIn: Dispatch<SetStateAction<boolean>>
+    setIsLoggedIn: Dispatch<SetStateAction<boolean>>,
+    userName: string,
+    setUserName: Dispatch<SetStateAction<string>>
     trackerID: number,
     setTrackerID: Dispatch<SetStateAction<number>>
 }
@@ -17,11 +22,14 @@ interface IGlobalContext {
 // @ts-ignore
 const ContextProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [userName, setUserName] = useState('')
     const [trackerID, setTrackerID] = useState(0)
 
     return <Context.Provider value={{
         isLoggedIn,
         setIsLoggedIn,
+        userName,
+        setUserName,
         trackerID,
         setTrackerID,
     }}>
--- frontend/src/utils/useFetch.ts
+++ frontend/src/utils/useFetch.ts
@@ -1,21 +1,29 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import { Context } from '../Context/ContextProvider'
+import { useHistory } from 'react-router-dom'
+
 
 const useFetch = <T extends any>(endpoint: string) => {
     const [response, setResponse] = useState<T>()
     const [error, setError] = useState('')
+    const context = useContext(Context)
+    const history = useHistory()
 
     useEffect(() => {
         let isSubscribed = true
         // Send all requests as superuser until we create concepts of users in frontend
         const data = {
             headers: {
-                'X-username': 'superuser',
+                'X-username': context.userName,
             },
         }
         const fetchData = async () => {
             try {
                 const res = await fetch(`${apiUrl}/${endpoint}`, data)
                 if (!res.ok) {
+                    context.setUserName('')
+                    context.setIsLoggedIn(false)
+                    history.push('login')
                     throw new Error(`${res.status} ${res.statusText}`)
                 }
                 const json = await res.json()
@@ -32,11 +40,11 @@ const useFetch = <T extends any>(endpoint: string) => {
         return () => {
             isSubscribed = false
         }
-    }, [endpoint])
+    }, [endpoint, context.userName, history, context])
 
     return { response, error }
 }
 
-export const apiUrl = 'https://api.trckpck.theodorc.no'
+export const apiUrl = 'http://api.trckpck.theodorc.no'
 
 export default useFetch
