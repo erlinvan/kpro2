6d10dd108e8c902815af046c155c49266034fae9
Erling Van De Weijer <erling.weijer@gmail.com>
2020-10-19 11:39:57 +0200

    Move Sign out and MyTrackers to the right side of the navbar to make the navbar less clustered

--- frontend/src/AppBar/AppBar.tsx
+++ frontend/src/AppBar/AppBar.tsx
@@ -6,36 +6,48 @@ import Toolbar from '@material-ui/core/Toolbar'
 import './AppBar.css'
 import { withRouter, useHistory } from 'react-router-dom'
 import { Context } from '../Context/ContextProvider'
+import { Grid } from '@material-ui/core'
 
 const AppBar = () => {
     const history = useHistory()
-
     const context = useContext(Context)
+
     return (
         <AppBarMUI position="static">
             <Toolbar>
-                <Typography variant="h6">
-                    trckpck
-                </Typography>
-                {
-                    context.isLoggedIn ?
-                        <Button color="inherit" onClick={() => {
-                            history.push('trackers')
-                        }}>
+                <Grid container>
+                    <Grid item xs>
+                        <Typography variant="h6">trckpck</Typography>
+                    </Grid>
+                    <Grid item xs={8}></Grid>
+                    {context.isLoggedIn && (
+                        <Button
+                            color="inherit"
+                            onClick={() => {
+                                history.push('trackers')
+                            }}
+                        >
                             <Typography variant="h6">My Trackers</Typography>
-                        </Button> : <></>
-                }
-                {
-                    context.isLoggedIn ?
-                        <Button color="inherit" onClick={() => {
-                            context.isLoggedIn && context.setIsLoggedIn(!context.isLoggedIn)
-                            history.push('login')
-                        }}>
-                            <Typography variant="h6">{context.isLoggedIn ? 'Sign out' : 'Sign in'}</Typography>
-                        </Button> : <></>
-                }
+                        </Button>
+                    )}
+                    {context.isLoggedIn && (
+                        <Grid item xs>
+                            <Button
+                                color="inherit"
+                                onClick={() => {
+                                    context.isLoggedIn &&
+                                        context.setIsLoggedIn(
+                                            !context.isLoggedIn
+                                        )
+                                    history.push('login')
+                                }}
+                            >
+                                <Typography variant="h6">Sign out</Typography>
+                            </Button>
+                        </Grid>
+                    )}
+                </Grid>
             </Toolbar>
-
         </AppBarMUI>
     )
 }
