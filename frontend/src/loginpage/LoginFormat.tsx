4b622c2d3b298d0244860cab19cb105fd9313310
Erling Van De Weijer <erling.weijer@gmail.com>
2020-10-21 16:53:57 +0200

    Refractor loginFormat components into login, as this will simplify the code

--- frontend/src/loginpage/LoginFormat.tsx
+++ frontend/src/loginpage/LoginFormat.tsx
@@ -1,21 +1,40 @@
-import React, { useState } from 'react'
-import UsernameComp from './UsernameComp'
-import LoginButtonUser from './LoginButton'
+import React, { useContext, useState } from 'react'
 import './LoginFormat.css'
-import { Grid } from '@material-ui/core'
-
+import { Grid, TextField } from '@material-ui/core'
+import Button from '@material-ui/core/Button/Button'
+import { useHistory } from 'react-router-dom'
+import { Context } from '../Context/ContextProvider'
 
 const LoginFormat = () => {
-
-
     const [userName, setUserName] = useState('')
+    const history = useHistory()
+    const context = useContext(Context)
+    const submit = () => {
+        context.setIsLoggedIn(true)
+        context.setUserName(userName)
+        history.push('trackers')
+    }
     return (
         <Grid className="LoginFormat">
-            <UsernameComp setUserName={setUserName} />
-
+            <TextField
+                id="standard-basic"
+                label="Username"
+                onKeyPress={(event) => {
+                    event.key === 'Enter' && submit()
+                }}
+                onChange={(event) => {
+                    setUserName('' + event.target.value)
+                }}
+            />
             <br></br>
-            <LoginButtonUser userName={userName} />
-
+            <Button
+                variant="contained"
+                onClick={() => {
+                    submit()
+                }}
+            >
+                Sign in
+            </Button>
         </Grid>
     )
 }
