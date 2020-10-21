9feb09cae0a8d9cd7d81ac38c20ba010a2828f96
Ask Sommervoll <asksom@gmail.com>
2020-10-21 11:34:29 +0200

    Make company searchable in grid

--- frontend/src/Components/TrackerSearch/SearchBarAndTitle.tsx
+++ frontend/src/Components/TrackerSearch/SearchBarAndTitle.tsx
@@ -14,6 +14,7 @@ const SearchBarAndTitle = ({ setSearchString }: props) => {
             label="Search for tracker"
             onChange={(event) => {
                 setSearchString('' + event.target.value)
+                isNaN(Number(event.target.value)) ? console.log('ja'): console.log('nei')
             }}
         />
     )
