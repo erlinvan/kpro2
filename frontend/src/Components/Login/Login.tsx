05090e027dbf04035ba77fca046283491f306cb4
Erling Van De Weijer <erling.weijer@gmail.com>
2020-10-21 17:55:31 +0200

    Show warning when entering a username that is invalid

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -1,20 +1,51 @@
 import React, { useContext, useState } from 'react'
 import './Login.css'
 import { Context } from '../../Context/ContextProvider'
-import { Button, Grid, TextField } from '@material-ui/core'
+import {
+    Button,
+    Collapse,
+    Grid,
+    IconButton,
+    TextField,
+} from '@material-ui/core'
 import { useHistory } from 'react-router-dom'
+import Alert from '@material-ui/lab/Alert'
+import CloseIcon from '@material-ui/icons/Close'
 
 const Login = () => {
     const [userName, setUserName] = useState('')
     const history = useHistory()
     const context = useContext(Context)
+    const [open, setOpen] = React.useState(true)
     const submit = () => {
         context.setIsLoggedIn(true)
         context.setUserName(userName)
+        context.setWarning('')
         history.push('trackers')
     }
     return (
         <Grid className="Login">
+            {context.warning !== '' && (
+                <Collapse in={open}>
+                    <Alert
+                        severity="warning"
+                        action={
+                            <IconButton
+                                aria-label="close"
+                                color="inherit"
+                                size="small"
+                                onClick={() => {
+                                    setOpen(false)
+                                }}
+                            >
+                                <CloseIcon fontSize="inherit" />
+                            </IconButton>
+                        }
+                    >
+                        {context.warning}
+                    </Alert>
+                </Collapse>
+            )}
             <TextField
                 id="standard-basic"
                 label="Username"
--- frontend/src/Context/ContextProvider.tsx
+++ frontend/src/Context/ContextProvider.tsx
@@ -4,19 +4,22 @@ export const Context = React.createContext<IGlobalContext>({
     isLoggedIn: false,
     setIsLoggedIn: () => {},
     userName: '',
-    setUserName:()=>{},
+    setUserName: () => {},
     trackerID: 0,
     setTrackerID: () => {},
-
+    warning: '',
+    setWarning: () => {},
 })
 
 interface IGlobalContext {
-    isLoggedIn: boolean,
-    setIsLoggedIn: Dispatch<SetStateAction<boolean>>,
-    userName: string,
+    isLoggedIn: boolean
+    setIsLoggedIn: Dispatch<SetStateAction<boolean>>
+    userName: string
     setUserName: Dispatch<SetStateAction<string>>
-    trackerID: number,
+    trackerID: number
     setTrackerID: Dispatch<SetStateAction<number>>
+    warning: string
+    setWarning: Dispatch<SetStateAction<string>>
 }
 
 // @ts-ignore
@@ -24,17 +27,24 @@ const ContextProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
     const [userName, setUserName] = useState('')
     const [trackerID, setTrackerID] = useState(0)
+    const [warning, setWarning] = useState('')
 
-    return <Context.Provider value={{
-        isLoggedIn,
-        setIsLoggedIn,
-        userName,
-        setUserName,
-        trackerID,
-        setTrackerID,
-    }}>
-        {children}
-    </Context.Provider>
+    return (
+        <Context.Provider
+            value={{
+                isLoggedIn,
+                setIsLoggedIn,
+                userName,
+                setUserName,
+                trackerID,
+                setTrackerID,
+                warning,
+                setWarning,
+            }}
+        >
+            {children}
+        </Context.Provider>
+    )
 }
 
 export default ContextProvider
