08e1b6bbd60dfba8a8381ae8fb982c32f08a212a
Ask Sommervoll <asksom@gmail.com>
2020-10-21 11:13:47 +0200

    Add polylines to historic map

--- frontend/src/Components/Trackerinfo/HistoricTrackerMap.tsx
+++ frontend/src/Components/Trackerinfo/HistoricTrackerMap.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Map, Marker, Polyline, Popup, TileLayer } from 'react-leaflet'
 import './TrackerInfo.css'
 
 
 import { IGPSData } from '../../Interfaces/ITrackers'
-
 
 
 type props = {
@@ -33,14 +32,33 @@ const HistoricTrackerMap = ({ data }: props) => {
         )
     }, [data])
 
+    const [polylines, setPolylines] = useState<any>([])
+
+    useEffect(() => {
+        let tempPolylines = []
+        for (let _i = 0; _i < data.length - 1; _i++) {
+            tempPolylines.push([data[_i], data[_i + 1]])
+        }
+        setPolylines(
+            tempPolylines.map((x) => {
+                return <Polyline
+                    positions={[
+                        [x[0].gps.lat, x[0].gps.lon], [x[1].gps.lat, x[1].gps.lon],
+                    ]}
+                />
+            }),
+        )
+    }, [data])
+
     return (
         <div>
-            <Map center={[64.9139, 18.7522]} zoom={4.8} className="leafletContainer">
+            <Map center={[64.9139, 18.7522]} zoom={4.8} className="leaflet-container">
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
                 {markers}
+                {polylines}
             </Map>
         </div>
     )
