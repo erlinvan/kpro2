56b8bb0609a8f1737f15747e3033bd11c59688e5
Erling Van De Weijer <erling.weijer@gmail.com>
2020-10-26 14:53:59 +0100

    Sort by id when searching for id, so that exact matchers will appear first in the list

--- frontend/src/Components/TrackerSearch/TrackerDataGrid.tsx
+++ frontend/src/Components/TrackerSearch/TrackerDataGrid.tsx
@@ -1,5 +1,10 @@
 import React, { useContext } from 'react'
-import { GridOverlay, DataGrid, ColDef } from '@material-ui/data-grid'
+import {
+    GridOverlay,
+    DataGrid,
+    ColDef,
+    SortDirection,
+} from '@material-ui/data-grid'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import './SearchAndTableComponent.css'
 import { useHistory } from 'react-router-dom'
@@ -13,6 +18,7 @@ const columns: ColDef[] = [
     { field: 'timestamp', headerName: 'Timestamp', width: 250 },
     { field: 'company', headerName: 'Company', width: 110 },
 ]
+
 function CustomLoadingOverlay() {
     return (
         <GridOverlay>
@@ -25,9 +31,10 @@ function CustomLoadingOverlay() {
 
 type props = {
     data: ITrackers[]
+    searching: boolean
 }
 
-const TrackerDataGrid = ({ data }: props) => {
+const TrackerDataGrid = ({ data, searching }: props) => {
     const history = useHistory()
     const context = useContext(Context)
 
@@ -35,20 +42,46 @@ const TrackerDataGrid = ({ data }: props) => {
         context.setTrackerID(id)
         history.push('trackerinfo')
     }
+    const sortModelId = [
+        {
+            field: 'id',
+            sort: 'asc' as SortDirection,
+        },
+    ]
+    const sortModelTimeStamp = [
+        {
+            field: 'timestamp',
+            sort: 'desc' as SortDirection,
+        },
+    ]
     return (
         <div className="trackerDataGrid">
             <Typography variant="h6" style={typographyStyle}>
                 Recently updated trackers
             </Typography>
-            <DataGrid
-                rows={data}
-                columns={columns}
-                pageSize={10}
-                onRowClick={(params) => handleClick(Number(params.data.id))}
-                components={{
-                    loadingOverlay: CustomLoadingOverlay,
-                }}
-            />
+            {searching ? (
+                <DataGrid
+                    sortModel={sortModelId}
+                    rows={data}
+                    columns={columns}
+                    pageSize={10}
+                    onRowClick={(params) => handleClick(Number(params.data.id))}
+                    components={{
+                        loadingOverlay: CustomLoadingOverlay,
+                    }}
+                />
+            ) : (
+                <DataGrid
+                    sortModel={sortModelTimeStamp}
+                    rows={data}
+                    columns={columns}
+                    pageSize={10}
+                    onRowClick={(params) => handleClick(Number(params.data.id))}
+                    components={{
+                        loadingOverlay: CustomLoadingOverlay,
+                    }}
+                />
+            )}
         </div>
     )
 }
