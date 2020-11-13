a2c69530f4504c3ea75921e2ba9cee565015d3f8
Erling Van De Weijer <erling.weijer@gmail.com>
2020-11-13 10:44:06 +0100

    Move Appbar to components folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
-import AppBar from './AppBar/AppBar'
 import Trackerinfo from './Components/Trackerinfo/Trackerinfo'
 import Login from './Components/Login/Login'
 import SearchAndTableComponent from './Components/TrackerSearch/SearchAndTableComponent'
 import ContextProvider from './Context/ContextProvider'
 import LandingPage from './Components/LandingPage/LandingPage'
+import AppBar from './Components/AppBar/AppBar'
 
 function App() {
     return (
